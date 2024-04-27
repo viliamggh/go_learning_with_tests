5145d4ae51df6b74816722001ff309e06cd47c59
Viliam Gago <viliam.gago95@gmail.com>
2024-04-27 17:42:36 +0200

    refactor: consts, default greeting, helper function

--- 1_hello_world/hello_test.go
+++ 1_hello_world/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' if no input is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, worl"
+		assertCorrectMessage(t, got, want)
+
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
